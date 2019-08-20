c169ad6fda27e9187b510739b78cfc4d74406700
leticiaenz <leticia.enz11@gmail.com>
2019-08-20 18:44:29 -0400

    feat: create global style

---
+++ src/styles/GlobalStyle.js
@@ -0,0 +1,17 @@
+import { createGlobalStyle } from "styled-components";
+
+const GlobalStyle = createGlobalStyle`
+  * {
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+  }
+
+  body{
+    background: #5688C7;
+    text-rendering: optimizeLegibility !important;
+    -webkit-font-smoothing: antialiased !important;
+  }
+`;
+
+export default GlobalStyle;
