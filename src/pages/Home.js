0869ff15c2fcc8afd00191c9705f8ab3bcdc5cef
leticiaenz <leticia.enz11@gmail.com>
2019-08-20 19:12:15 -0400

    feat: update component home to execute the query

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,5 +1,41 @@
-import React from "react";
+import React, { Component } from "react";
+import graphql from "babel-plugin-relay/macro";
+import { QueryRenderer } from "react-relay";
+import { Environment } from "../relay";
 
-const Home = () => <h1>Home</h1>;
+import PokemonList from "../components/PokemonList";
+import Loading from "../components/Loading";
+
+class Home extends Component {
+  render() {
+    return (
+      <QueryRenderer
+        environment={Environment}
+        query={HomeQuery}
+        render={({ error, props }) => {
+          if (error) {
+            return (
+              <div>
+                <h1>Aconteceu algum erro</h1>
+                <h1>{error.message}</h1>
+              </div>
+            );
+          } else if (props) {
+            return <PokemonList {...props} />;
+          }
+          return <Loading />;
+        }}
+      />
+    );
+  }
+}
+
+const HomeQuery = graphql`
+  query HomeQuery {
+    pokemons(first: 25) {
+      ...PokemonList_pokemons
+    }
+  }
+`;
 
 export default Home;
