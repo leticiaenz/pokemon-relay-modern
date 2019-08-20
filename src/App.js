cb4835b9f31fd89090fd40d7f78f261f1f0e0b1f
leticiaenz <leticia.enz11@gmail.com>
2019-08-20 18:51:19 -0400

    feat: configure router and add home and details pages

--- src/App.js
+++ src/App.js
@@ -1,42 +1,15 @@
-import React, { Component } from "react";
-import graphql from "babel-plugin-relay/macro";
-import { QueryRenderer } from "react-relay";
+import React from "react";
+import { BrowserRouter, Route } from "react-router-dom";
+import GlobalStyle from "./styles/GlobalStyle";
+import Home from "./pages/Home";
+import Details from "./pages/Details";
 
-import { Environment } from "./relay";
+const App = () => (
+  <BrowserRouter>
+    <GlobalStyle />
+    <Route path="/" exact component={Home} />
+    <Route path="/details/:pokemonID" component={Details} />
+  </BrowserRouter>
+);
 
-const App = ({ query }) => {
-  console.log(query);
-  return <h1>Pokemon</h1>;
-};
-
-const AppQR = () => {
-  return (
-    <QueryRenderer
-      environment={Environment}
-      query={graphql`
-        query AppQuery {
-          pokemons(first: 20) {
-            id
-            name
-            image
-          }
-        }
-      `}
-      variables={{}}
-      render={({ error, props }) => {
-        console.log("qr: ", error, props);
-        if (error) {
-          return <span>{error.toString()}</span>;
-        }
-
-        if (props) {
-          return <App query={props} />;
-        }
-
-        return <span>loading</span>;
-      }}
-    />
-  );
-};
-
-export default AppQR;
+export default App;
